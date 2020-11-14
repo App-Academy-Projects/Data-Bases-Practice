1085bc4c0f22436cc2c5516ec2a8f0fd0ea05c89
usamaeltmsah <usamaeltmsah@gmail.com>
2020-11-14 17:08:32 +0200

    For each continent show the continent and number of countries

--- lib/05_aggregates.rb
+++ lib/05_aggregates.rb
@@ -68,6 +68,12 @@ end
 def country_counts
   # For each continent show the continent and number of countries.
   execute(<<-SQL)
+    SELECT DISTINCT
+      continent, COUNT(*) AS number_of_countries
+    FROM
+      countries
+    GROUP BY 
+      continent
   SQL
 end
 
