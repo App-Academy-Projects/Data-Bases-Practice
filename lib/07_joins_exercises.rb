0d93fa10fc6baa2d63725b401ce7f345cdf56ea0
usamaeltmsah <usamaeltmsah@gmail.com>
2020-11-14 22:30:51 +0200

    List the film title and the leading actor for all of the films 'Julie Andrews' played in

--- lib/07_joins_exercises.rb
+++ lib/07_joins_exercises.rb
@@ -112,6 +112,27 @@ def andrews_films_and_leads
   # List the film title and the leading actor for all of the films 'Julie
   # Andrews' played in.
   execute(<<-SQL)
+    SELECT
+      movies.title, actors.name
+    FROM
+      movies
+    JOIN
+      castings ON movies.id = castings.movie_id
+    JOIN
+      actors ON castings.actor_id = actors.id
+    WHERE
+      castings.ord = 1 AND movies.id IN (
+        SELECT
+          movies.id
+        FROM
+          movies
+        JOIN
+          castings ON movies.id = castings.movie_id
+        JOIN
+          actors ON castings.actor_id = actors.id
+        WHERE
+          actors.name = 'Julie Andrews'
+      )
   SQL
 end
 
