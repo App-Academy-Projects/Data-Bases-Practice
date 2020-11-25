d397a65d164cde77f1e3fdfc0a0f065907c2927a
usamaeltmsah <usamaeltmsah@gmail.com>
2020-11-25 19:07:05 +0200

    Get all Playwrights from database

--- 02. Plays-Playwrights ORM/playwright.rb
+++ 02. Plays-Playwrights ORM/playwright.rb
@@ -1,6 +1,11 @@
 require_relative 'plays.rb'
 
 class Playwright
+    def self.all
+        data = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
+        data.map { |datum| Playwright.new(datum) }
+    end
+
     def initialize(options)
         @id = options['id']
         @name = options['name']
